ace3bb41670d920a6b0b54e823155a63328ad9da
Geraldo <geraldoaax@gmail.com>
2019-07-04 22:33:57 -0300

    up

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,16 @@ import { createStore, compose, applyMiddleware } from "redux";
 
 import reducers from "./reducers";
 //Passar os reducers aqui
-const store = createStore(reducers, );
+
+//toda action disparada na aplicação vai para o Reactron.
+const composer =
+  process.env.NODE_ENV === "development"
+    ? compose(
+        applyMiddleware(...[]),
+        console.tron.createEnhancer()
+      )
+    : applyMiddleware(...[]);
+
+const store = createStore(reducers, composer);
 
 export default store;
